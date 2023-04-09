fa1b6ac38bcbb4095128865f4f029e858f8fa69a
Kaa <>
2023-04-09 11:00:35 +0200

    Emerging the assembly directory

--- src/assembly/mod.rs
+++ src/assembly/mod.rs
@@ -1,3 +1,5 @@
+use std::process::{Command, Stdio};
+
 use crate::prelude::*;
 
 pub fn emerge_irfs(assembly_target: &str) {
@@ -5,7 +7,44 @@ pub fn emerge_irfs(assembly_target: &str) {
     shrun(&ShellCommand::new("rm").args(["-rf", assembly_target]));
     shrun(&ShellCommand::new("mkdir").args(["-p", assembly_target]));
 
+    let cwd = std::env::current_dir().expect("Failed to find current direcotry");
     let profile = get_profile();
+
+    std::env::set_current_dir(assembly_target).expect("Failed to change directory");
+    shrun(&ShellCommand::new("mkdir").args(["dev"]));
+    shrun(&ShellCommand::new("cp").args(["-a", "/dev/null", "dev"]));
+    std::env::set_current_dir(&cwd).expect("Failed to change directory");
+
+    shrun(&ShellCommand::new("eselect").args(["profile", "set", "1"]));
+
+    emerge_pkg(assembly_target);
+
+    shrun(&ShellCommand::new("eselect").args(["profile", "set", &profile.to_string()]));
+}
+
+fn emerge_pkg(t: &str) {
+    Command::new("emerge")
+        .env("USE", "lvm kernel -openssl")
+        .args([
+            "-vnq",
+            "--keep-going=y",
+            "--ignore-world=y",
+            "--deep",
+            &format!("--root={}", t),
+            "sys-apps/busybox",
+            "sys-fs/cryptsetup",
+            "sys-fs/lvm2",
+            "app-crypt/gnupg",
+            "app-shells/bash",
+            "app-misc/pax-utils",
+        ])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("Could not spawn emerge")
+        .wait()
+        .expect("Failed emerge of initramfs programs");
 }
 
 fn get_profile() -> u8 {
